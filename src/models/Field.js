050f7da702cf086e06f960ed7d5404ad47a0a772
Shawn Mealey <mealeyst@gmail.com>
2016-07-26 10:00:34 -0400

    Adding save function and 'type' prop to Field
    Adding the `save()` function to the Field class to return a JSON string
    that can be used when saving ContentType configurations to file. NOTE:
    this function itself doesn't save the Field class attributes to file. I
    also added the required constructor property of `type` that all Field
    child classes must provide.

--- src/models/Field.js
+++ src/models/Field.js
@@ -1,7 +1,7 @@
 export default class Field {
   constructor (props) {
     if (typeof props === 'object') {
-      const requiredProps = ['name', 'uuid']
+      const requiredProps = ['name', 'uuid', 'type']
       requiredProps.map((prop, index) => {
         if (props.hasOwnProperty(prop)) {
           this[prop] = props[prop]
@@ -9,8 +9,17 @@ export default class Field {
           throw new Error('Error: Missing required property')
         }
       })
+      if (props.hasOwnProperty('length') && props.length > 0) {
+        this.length = props.length
+      } else {
+        // TODO: replace this with config settings
+        this.length = 37
+      }
     } else {
       throw new Error('Error: Expecting typeof Object')
     }
+  }
+  save () {
+    return JSON.stringify(this)
   }
 }
